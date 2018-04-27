e69ecc7387e50bbbb3874a88c8eb644bf4b8e75c
ice1000 <ice1000@sbrella.com>
2018-04-27 19:43:18 +0800

    Update gradle script

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,34 +5,41 @@ val kotlinVersion = "1.2.40"
 group = "me.serce.devkt.solidity.lang"
 version = "v1.0"
 
-plugins { java }
+plugins {
+	java
+	application
+}
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_1_8
-  targetCompatibility = JavaVersion.VERSION_1_8
+	sourceCompatibility = JavaVersion.VERSION_1_8
+	targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 java.sourceSets {
-  "main" {
-    java.setSrcDirs(listOf("src", "gen"))
-    resources.setSrcDirs(listOf("res"))
-  }
+	"main" {
+		java.setSrcDirs(listOf("src", "gen"))
+		resources.setSrcDirs(listOf("res"))
+	}
 
-  "test" {
-    java.setSrcDirs(emptyList<Any>())
-    resources.setSrcDirs(emptyList<Any>())
-  }
+	"test" {
+		java.setSrcDirs(emptyList<Any>())
+		resources.setSrcDirs(emptyList<Any>())
+	}
 }
 
 repositories {
-  mavenCentral()
-  jcenter()
-  maven("https://jitpack.io")
+	mavenCentral()
+	jcenter()
+	maven("https://jitpack.io")
+}
+
+application {
+	mainClassName = "org.ice1000.devkt.Main"
 }
 
 dependencies {
-  compileOnly(kotlin("compiler-embeddable", kotlinVersion))
-	val version = "fffe21c45d"
+	compileOnly(kotlin("compiler-embeddable", kotlinVersion))
+	val version = "v1.4"
 	compileOnly(group = "com.github.ice1000.dev-kt", name = "common", version = version)
 	runtime(group = "com.github.ice1000.dev-kt", name = "swing", version = version)
 }
